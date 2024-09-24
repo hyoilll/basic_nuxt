640c1d35109a3b5a21e7c6062df64b2837b5a78e
hyoilll <dlgydlf12345@naver.com>
2024-09-25 01:03:48 +0900

    global scss設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  css: ['~/assets/scss/main.scss'],
   vite: {
     vue: {
       customElement: true
     },
     vueJsx: {
       mergeProps: true
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/colors.scss" as *;'
+        }
+      }
     }
-  }
+  },
 })

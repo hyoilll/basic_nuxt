24cd590eecbbf83710507fa3b46ee4efac42cd43
hyoilll <dlgydlf12345@naver.com>
2024-09-27 00:08:56 +0900

    route navigation

---
+++ pages/posts/[id].vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+    <p>This page will be displayed at the /post/{{ routeId }} route.</p>
+  </section>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute()
+const routeId = route.params.id
+</script>

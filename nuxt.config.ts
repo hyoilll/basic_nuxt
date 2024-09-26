a38da1feb407a259feb9753867287bedeb58ecf7
hyoilll <dlgydlf12345@naver.com>
2024-09-26 23:53:56 +0900

    install tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,4 +18,7 @@ export default defineNuxtConfig({
       }
     }
   },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ]
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Welcome to the homepage index</h1>
+    <h1 class="text-red-500 text-lg font-bold">Welcome to the homepage index</h1>
     <AppAlert>
       This is an auto-imported component
     </AppAlert>

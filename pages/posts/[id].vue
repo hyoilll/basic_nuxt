149e058bf3b6277dcbe9d27c0aea037baaaddc6e
hyoilll <dlgydlf12345@naver.com>
2024-09-27 00:27:32 +0900

    route validate

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -7,4 +7,19 @@
 <script lang="ts" setup>
 const route = useRoute()
 const routeId = route.params.id
+
+definePageMeta({
+  // ページごとにルートの検証を行う
+  validate: async (route) => {
+    /**
+     * ページが表示される前に、そのルートが有効かどうかをチェックする
+     * false を返すと、そのページは表示されず、Nuxtは次のマッチングを試みます。それでも適切なページが見つからない場合、404エラーページが表示されます。
+     * 「Nuxtは次のマッチングを試みます」とは、NuxtがそのURLに対して別のページが適合しないかどうかを確認することを意味します。
+     * 例えば、/posts/1の場合には1が数字なので、問題なくvalidateがtrueになって、ページが表示される。一方、/posts/testの場合にはtestが数字ではないので、validateがfalseになり、ページが表示されない。
+     * よって、nuxtはここで諦めるのではなく、/posts/testのルートが本当にあるかどうか試みる。もし、存在してたら/posts/testのコンポーネントをレンダリングする。
+     * しかし、存在してなかったら、そのまま404エラーを表示する。
+     */
+    return typeof route.params.id === 'string' && /^\d+$/.test(route.params.id) // Check if the id is made up of digits
+  }
+})
 </script>

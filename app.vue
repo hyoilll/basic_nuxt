de0d325716725635935a4ebd952a3332084728af
hyoilll <dlgydlf12345@naver.com>
2024-09-28 15:50:00 +0900

    useSeoMeta

--- app.vue
+++ app.vue
@@ -5,18 +5,17 @@
 </template>
 
 <script setup lang="ts">
-useHead({
-  title: 'My App',
-  // <meta> タグを追加して、description として "My amazing site." という説明文を設定しています。これはSEO（検索エンジン最適化）のために使われる。
-  // meta descriptionは、ページの説明を検索エンジンに提供する重要なタグです。この情報は、検索結果ページ（SERP）でページの概要として表示されることが多いです。検索エンジンはこの説明を元に、ページの内容を把握し、ユーザーに最適な結果を表示。
-  meta: [
-    { name: 'description', content: 'My amazing site.' }
-  ],
-  // <body> タグにクラスを追加しています。具体的には class="test" という属性が追加されます。
-  bodyAttrs: {
-    class: 'test'
-  },
-  // ページにインラインスクリプトを追加しています。このスクリプトは、ページが読み込まれた際に console.log('Hello world') を実行。
-  script: [{ innerHTML: 'console.log(\'Hello world\')' }]
+/**
+ * useSeoMeta は、useHead の代替として使うことができる。
+ * SEOメタタグ の定義に特化しており、簡潔に設定できるよう設計されている。
+ * また、TypeScript のサポートも強化されており、オブジェクト形式でタグを定義するため、間違いを防ぐgことができる。
+ */
+useSeoMeta({
+  title: 'My Amazing Site',
+  ogTitle: 'My Amazing Site',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
 })
 </script>

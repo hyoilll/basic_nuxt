7cc1ab496de248261e3cb307c2b9f22eda381508
hyoilll <dlgydlf12345@naver.com>
2024-09-28 15:51:19 +0900

    meta tag Component

--- app.vue
+++ app.vue
@@ -1,21 +1,15 @@
 <template>
+  <Head>
+    <Title>{{ title }}</Title>
+    <Meta name="description" :content="title" />
+    <Style type="text/css" children="body { background-color: green; }"></Style>
+  </Head>
+
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-/**
- * useSeoMeta は、useHead の代替として使うことができる。
- * SEOメタタグ の定義に特化しており、簡潔に設定できるよう設計されている。
- * また、TypeScript のサポートも強化されており、オブジェクト形式でタグを定義するため、間違いを防ぐgことができる。
- */
-useSeoMeta({
-  title: 'My Amazing Site',
-  ogTitle: 'My Amazing Site',
-  description: 'This is my amazing site, let me tell you all about it.',
-  ogDescription: 'This is my amazing site, let me tell you all about it.',
-  ogImage: 'https://example.com/image.png',
-  twitterCard: 'summary_large_image',
-})
+const title = ref('Hello World')
 </script>
